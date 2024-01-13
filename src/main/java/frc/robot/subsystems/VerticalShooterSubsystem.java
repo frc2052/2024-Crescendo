ffcd73e8f2f51ec8cf9189ccf0b1ef65938b88a3
Max <nosnaws1019@gmail.com>
2024-01-13 10:26:29 -0600

    wierd bug

---
+++ src/main/java/frc/robot/subsystems/VerticalShooterSubsystem.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems;
+
+public class VerticalShooterSubsystem extends SubsytemBase {
+    
+    private final falcon lowerMotor = new falcon();
+    private final falcon upperMotor = new falcon();
+
+
+
+}
