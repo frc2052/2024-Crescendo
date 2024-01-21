862e17ac8e83362132c7aa3e6630aa0a27303a9e
Maxwell Swanson <nosnaws1019@gmail.com>
2024-01-21 16:15:01 -0600

    changed names

--- src/main/java/frc/robot/commands/VerticalShooterCustomShotCommands.java
+++ src/main/java/frc/robot/commands/VerticalShooterCustomShotCommands.java
@@ -3,10 +3,10 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.shooter.VerticalShooterSubsystem;
 
-public class VerticalShooterMidRangeShotCommands extends Command {
+public class VerticalShooterCustomShotCommands extends Command {
 private VerticalShooterSubsystem shooter;
 
-public VerticalShooterMidRangeShotCommands(VerticalShooterSubsystem shooter) {
+public VerticalShooterCustomShotCommands(VerticalShooterSubsystem shooter) {
     this.shooter = shooter;
     addRequirements(shooter);
 }
