87201faa61ba5053b9308044ac70ec59126546cd
Max <nosnaws1019@gmail.com>
2024-01-27 11:00:11 -0600

    New Commands

--- src/main/java/frc/robot/commands/ShooterAmpSpeedCommand.java
+++ src/main/java/frc/robot/commands/ShooterAmpSpeedCommand.java
@@ -4,18 +4,18 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.shooter.VerticalShooterSubsystem;
 import frc.robot.subsystems.shooter.VerticalShooterSubsystem.ShooterSpeeds;
 
-public class ShooterIdleSpeedCommand extends Command{
+public class ShooterAmpSpeedCommand extends Command{
 
     private final VerticalShooterSubsystem shooter;
 
-    public ShooterIdleSpeedCommand(VerticalShooterSubsystem shooter) {
+    public ShooterAmpSpeedCommand(VerticalShooterSubsystem shooter) {
         this.shooter = shooter;
         addRequirements(shooter);
     }
 
     @Override
     public void initialize() {
-        shooter.setSpeed(ShooterSpeeds.AMP);
+        shooter.setSpeed(ShooterSpeeds.IDLING);
     }
 
     @Override
--- src/main/java/frc/robot/commands/ShooterSpeakerSpeedCommand.java
+++ src/main/java/frc/robot/commands/ShooterSpeakerSpeedCommand.java
@@ -4,18 +4,18 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.shooter.VerticalShooterSubsystem;
 import frc.robot.subsystems.shooter.VerticalShooterSubsystem.ShooterSpeeds;
 
-public class ShooterIdleSpeedCommand extends Command{
+public class ShooterSpeakerSpeedCommand extends Command{
 
     private final VerticalShooterSubsystem shooter;
 
-    public ShooterIdleSpeedCommand(VerticalShooterSubsystem shooter) {
+    public ShooterSpeakerSpeedCommand(VerticalShooterSubsystem shooter) {
         this.shooter = shooter;
         addRequirements(shooter);
     }
 
     @Override
     public void initialize() {
-        shooter.setSpeed(ShooterSpeeds.AMP);
+        shooter.setSpeed(ShooterSpeeds.SPEAKER);
     }
 
     @Override
